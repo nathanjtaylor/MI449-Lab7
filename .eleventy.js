010a05679ad4bd40f60420cf5ba147422a62b0fa
nathanjtaylor <144626570+nathanjtaylor@users.noreply.github.com>
2025-02-24 15:08:12 +0000

    update

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,16 @@
-
 // The export statement makes these settings available to other files in 11ty
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
+
+  eleventyConfig.addShortcode("userCard", function(user) {
+    return `
+      <div class="p-4 border rounded shadow mb-4">
+        <h2 class="text-xl font-semibold">${user.name}</h2>
+        <p>${user.email}</p>
+        <p>${user.phone}</p>
+        <p>${user.website}</p>
+      </div>
+    `;
+  });
 };
-
-
-<div class="p-4 border rounded shadow mb-4">
-  <h2 class="text-xl font-semibold" x-text="user.name"></h2>
-  <p x-text="user.email"></p>
-  <p x-text="user.phone"></p>
-  <p x-text="user.website"></p>
-</div>
