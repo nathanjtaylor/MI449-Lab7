448e1fc4a1cb0c28276d733058fec6cdcab76653
nathanjtaylor <144626570+nathanjtaylor@users.noreply.github.com>
2025-02-24 15:01:24 +0000

    start

--- .eleventy.js
+++ .eleventy.js
@@ -4,3 +4,11 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy("styles");
 };
+
+
+<div class="p-4 border rounded shadow mb-4">
+  <h2 class="text-xl font-semibold" x-text="user.name"></h2>
+  <p x-text="user.email"></p>
+  <p x-text="user.phone"></p>
+  <p x-text="user.website"></p>
+</div>
